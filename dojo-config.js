c2df912cf6e9c936c75390d93221f1c167ca834f
lroche <lroche.github@gmail.com>
2017-12-07 10:32:36 +0100

    Modified conf to be compliant with dojo >1.10

--- dojo-config.js
+++ dojo-config.js
@@ -22,7 +22,7 @@ var dojoConfig = {
         welcomeMessage: "Hello World !"
     },
     has:{    	
-    	"dojo-undef-api":1,
+        "dojo-undef-api":1
     },
     //Dojo Trace API 
     //for debugging :
@@ -32,6 +32,13 @@ var dojoConfig = {
     },
     //callback used when specified deps will be loaded:
     callback:null, //updated by karma-dojo-adapter
-
+    loaderPatch: {
+        undef: function(moduleId, referenceModule){
+			// In order to reload a module, it must be undefined (this routine) and then re-requested.
+			// This is useful for testing frameworks (at least).
+			delete require.modules[moduleId];
+			
+		}
+    },
     locale:"en"
 }
